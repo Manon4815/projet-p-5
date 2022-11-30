a545e73349e85bb1bfdffd542f77a69cce9a7604
Manon4815 <manette4815@gmail.com>
2022-11-30 15:28:48 +0100

    js changement

--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,11 @@
 fetch("http://localhost:3000/api/products").then(response => {
     return response.json()
 }).then(data => {
-    console.log(data)
-})
+    console.log(data);
+    let affichage ='<ul>';
+    for(let object of data){
+        affichage +='${object.imgURL}'
+    }
+    affichage += '</ul>';
+    document.querySelector("#object").innerHTML = affichage;
+});
