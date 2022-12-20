a32b9f2a92544e1cd9f0c9a3d3b9d690693c1b5c
Manon4815 <manette4815@gmail.com>
2022-12-20 11:19:01 +0100

    api javascript

--- front/js/script.js
+++ front/js/script.js
@@ -1,10 +1,27 @@
-fetch("http://localhost:3000/api/products").then(response => {
-    return response.json()
-}).then(data => {
-    console.log(data);
-    if(data.ok){
-        response.json().then(data => {
-            imgURL.src =data[0].url
-        })
-    }
-});
+const productsContainer = document.querySelector(".titles");
+let products =[];
+
+async function fetchProduits(){ 
+    await fetch("http://localhost:3000/api/products").then(response => {
+        return response.json()
+    }).then(data => (products = data));
+        console.log(products) 
+        for(let i = 0; i < products.length; i++){
+         console.log(products[i])
+        }
+    productsDisplay();
+}
+
+function productsDisplay(){
+ productsContainer.innerHTML = products.map((sofa) => 
+ `
+    <div class="card>
+    <img src=${sofa.imageUrl.jpeg} alt="sofa ${sofa.name} >
+    <h2>${sofa.name}</h2>
+    <h4>${sofa.price}</h4>
+    </div>
+ `)
+ .join("");
+}
+
+window.addEventListener("load", fetchProduits);
